0f87ca1b2badbe6cfa43eac304a320090e747b4c
akira4n <yobiijo94@gmail.com>
2026-06-16 10:38:04 +0700

    feat: add post routes

---
+++ src/routes/post.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const postController = require("../controllers/post.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.post("/posts", authMiddleware, postController.createPost);
+router.get("/posts", authMiddleware, postController.getAllPosts);
+router.get("/posts/:id", authMiddleware, postController.getPostById);
+
+module.exports = router;

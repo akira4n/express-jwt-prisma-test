a0d94e926beb9b58fb4ba2419463e0446432fef7
akira4n <yobiijo94@gmail.com>
2026-06-18 14:10:10 +0700

    feat: add deletePost route to post routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/posts", authMiddleware, postController.createPost);
 router.get("/posts", authMiddleware, postController.getAllPosts);
 router.get("/posts/:id", authMiddleware, postController.getPostById);
+router.delete("/posts/:id", authMiddleware, postController.deletePost);
 
 module.exports = router;
